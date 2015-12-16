8819279d69e1d8dba7689389c04a89131ccd78fb
Jorge Maroto <patoroco@gmail.com>
2015-12-16 22:27:09 +0100

    First commit

---
+++ SwiftBit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBit
+//
+//  Created by Jorge Maroto García on 16/12/15.
+//  Copyright © 2015 Jorge Maroto García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
